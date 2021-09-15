c3996214fd242fe8f74c7acdf5101c4b7105ac6c
yaekoboru <yaekoboru@gmail.com>
2021-09-15 16:16:03 -0400

    added logic file

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -59,7 +59,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 /// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+let torontoHoods = "https://raw.githubusercontent.com/yaekobboru/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 // // Grabbing our GeoJSON data.
 // d3.json(airportData).then(function(data) {
 //   console.log(data);
@@ -75,16 +75,10 @@ let myStyle = {
 
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data,{
-  style:myStyle,
-  onEachFeature:function(feature,layer){
-    layer.bindPopup("<h3> Airline:" + feature.properties.airline + "</h3> <hr><h3> Destination:"
-      + feature.properties.dst + "</h3>");
-  }
-}).addTo(map);
+L.geoJson(data).addTo(map);
 });
 
 
