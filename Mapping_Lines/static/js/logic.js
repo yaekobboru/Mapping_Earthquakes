cca595e5077d3c0e9db9564f185ce0e899df75ca
yaekoboru <yaekoboru@gmail.com>
2021-09-15 13:17:00 -0400

    add folders to repo

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -26,22 +26,26 @@ L.polyline(line, {
   color: "red"
 }).addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
+// // Get data from cities.js
+// let cityData = cities;
 
- // Loop through the cities array and create one marker for each city.
- cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location,{radius : city.population/100000})
-     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()  + "</h3>")
-     .addTo(map);
-   });
+//  // Loop through the cities array and create one marker for each city.
+//  cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location,{radius : city.population/100000})
+//      .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()  + "</h3>")
+//      .addTo(map);
+//    });
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// // We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    tileSize : 256,
+
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
     maxZoom: 18,
     accessToken: API_KEY
+   
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
