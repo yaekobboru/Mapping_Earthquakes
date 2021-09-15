0e0d984724ee9a6ba508b628a7c5c5ae2675cc2f
yaekoboru <yaekoboru@gmail.com>
2021-09-15 15:52:10 -0400

    created style for lines and added popup display

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -61,12 +61,54 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/yaekobboru/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
 
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/yaekobboru/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data).addTo(map);
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+
+
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data,{
+  style:myStyle,
+  onEachFeature:function(feature,layer){
+    layer.bindPopup("<h3> Airline:" + feature.properties.airline + "</h3> <hr><h3> Destination:"
+      + feature.properties.dst + "</h3>");
+  }
+}).addTo(map);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // Grabbing our GeoJSON data.
 // L.geoJson(sanFranAirport, {
